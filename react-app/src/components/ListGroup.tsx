b93b19ddaaa5ab95a67dd9b8e777e8a96786e24a
jonathan patelski <Jonathanpatelski@gmail.com>
2024-05-21 11:50:38 +0200

    onlclick function

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,21 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log(item)}
+            onClick={handleClick}
           >
             {item}
           </li>
