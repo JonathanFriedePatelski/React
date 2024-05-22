2fcb0a0fea1b25619a2d31e9220fc7cad4dbf066
jonathan patelski <Jonathanpatelski@gmail.com>
2024-05-22 09:43:05 +0200

    show/close alert button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,14 @@
 import Knop from "./components/Knop";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-       <Knop color="secondary" onClick={() => console.log('clicked')}>My button</Knop>
+       { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+       <Knop color="secondary" onClick={() => setAlertVisibility(true)}>My button</Knop>
     </div>
   );
 }
